30677258b8db67cc0485669b46f9544955a49b9e
Gabriel Dias <gabriel.dias@minastc.com.br>
2026-04-05 20:26:20 -0300

    fix: correcao de tags para next

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -20,7 +20,17 @@ export function ContactForm() {
         Preencha o formulário para entrar em contato
       </h2>
       <div className="p-5" data-aos="fade-right" data-aos-duration="1500">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1910581.2908260615!2d-46.051708758627626!3d-20.72629580853821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa693871c22c475%3A0xf03faac3a713c584!2sANMA%20DISTRIBUIDORA!5e0!3m2!1spt-BR!2sbr!4v1775418516310!5m2!1spt-BR!2sbr" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+       
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1910581.2908260615!2d-46.051708758627626!3d-20.72629580853821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa693871c22c475%3A0xf03faac3a713c584!2sANMA%20DISTRIBUIDORA!5e0!3m2!1spt-BR!2sbr!4v1775418516310!5m2!1spt-BR!2sbr"
+              width="600"
+              height="450"
+            allowFullScreen
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+
       </div >
     </motion.div>
   );
